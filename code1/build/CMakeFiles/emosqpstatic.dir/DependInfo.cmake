53495fd9c9a875a221e94820a18a8f1dd21567f9
Ricardo Pérez Ibacache <ricardo@macmini.local>
2020-08-19 11:52:52 -0300

    submit code in c

---
+++ code1/build/CMakeFiles/emosqpstatic.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/ricardo/Dropbox/2018/PUCRS/Papers/OSQP_Cmake/example_1/code1/src/osqp/auxil.c" "/Users/ricardo/Dropbox/2018/PUCRS/Papers/OSQP_Cmake/example_1/code1/build/CMakeFiles/emosqpstatic.dir/src/osqp/auxil.c.o"
+  "/Users/ricardo/Dropbox/2018/PUCRS/Papers/OSQP_Cmake/example_1/code1/src/osqp/lin_alg.c" "/Users/ricardo/Dropbox/2018/PUCRS/Papers/OSQP_Cmake/example_1/code1/build/CMakeFiles/emosqpstatic.dir/src/osqp/lin_alg.c.o"
+  "/Users/ricardo/Dropbox/2018/PUCRS/Papers/OSQP_Cmake/example_1/code1/src/osqp/osqp.c" "/Users/ricardo/Dropbox/2018/PUCRS/Papers/OSQP_Cmake/example_1/code1/build/CMakeFiles/emosqpstatic.dir/src/osqp/osqp.c.o"
+  "/Users/ricardo/Dropbox/2018/PUCRS/Papers/OSQP_Cmake/example_1/code1/src/osqp/proj.c" "/Users/ricardo/Dropbox/2018/PUCRS/Papers/OSQP_Cmake/example_1/code1/build/CMakeFiles/emosqpstatic.dir/src/osqp/proj.c.o"
+  "/Users/ricardo/Dropbox/2018/PUCRS/Papers/OSQP_Cmake/example_1/code1/src/osqp/qdldl.c" "/Users/ricardo/Dropbox/2018/PUCRS/Papers/OSQP_Cmake/example_1/code1/build/CMakeFiles/emosqpstatic.dir/src/osqp/qdldl.c.o"
+  "/Users/ricardo/Dropbox/2018/PUCRS/Papers/OSQP_Cmake/example_1/code1/src/osqp/qdldl_interface.c" "/Users/ricardo/Dropbox/2018/PUCRS/Papers/OSQP_Cmake/example_1/code1/build/CMakeFiles/emosqpstatic.dir/src/osqp/qdldl_interface.c.o"
+  "/Users/ricardo/Dropbox/2018/PUCRS/Papers/OSQP_Cmake/example_1/code1/src/osqp/scaling.c" "/Users/ricardo/Dropbox/2018/PUCRS/Papers/OSQP_Cmake/example_1/code1/build/CMakeFiles/emosqpstatic.dir/src/osqp/scaling.c.o"
+  "/Users/ricardo/Dropbox/2018/PUCRS/Papers/OSQP_Cmake/example_1/code1/src/osqp/util.c" "/Users/ricardo/Dropbox/2018/PUCRS/Papers/OSQP_Cmake/example_1/code1/build/CMakeFiles/emosqpstatic.dir/src/osqp/util.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
